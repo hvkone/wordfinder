133a7f9cb85cc8533f6bffee537adc8c8d69f982
hvkone <wiljevaiv@outlook.com>
2021-04-15 22:35:06 -0500

    List Tables in DB

--- sql/cnx_mysql.py
+++ sql/cnx_mysql.py
@@ -1,7 +1,7 @@
 import mysql.connector
 from mysql.connector import errorcode
 import pymysql
-from mysql.connector.constants import ClientFlag
+
 
 # Obtain connection string information from the portal
 db_config = {
@@ -10,7 +10,8 @@ db_config = {
     'user': 'adminteam@psd-wordfinder',
     'password': 'jFq&T7bPJXmY',
     #'client_flags': [mysql.connector.ClientFlag.SSL],
-    'ssl_ca': './/src//train//DigiCertGlobalRootG2.crt.pem'
+    #'ssl_ca': './/src//train//DigiCertGlobalRootG2.crt.pem' #vscode
+    'ssl_ca': 'DigiCertGlobalRootG2.crt.pem' #pycharm
 }
 
 # Construct connection string
@@ -18,7 +19,11 @@ try:
     #conn = mysql.connector.connect(**db_config)
     conn = pymysql.connect(**db_config)
     if conn:
-        print("Database psd_project was located.")
+        print("\n Database psd_project was located. \n \n List of tables in psd_project \n")
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'psd_project'")
+            print(cursor.fetchall())
+    
         
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
