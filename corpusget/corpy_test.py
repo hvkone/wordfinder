7fdc816bc517d74b86217b14165edf7031ab404c
evanswj <willie.evans@slu.edu>
2021-04-23 10:44:12 -0500

    removed all corpus txt files

--- corpusget/corpy_test.py
+++
@@ -1,8 +0,0 @@
-from corpy.udpipe import Model
-from corpy.udpipe import pprint
-
-
-m = Model(".//corpus//udpipemodel//chinese.udpipe")
-
-sents = list(m.process(".//corpus//chinese//23825-0.txt"))
-pprint(sents)
